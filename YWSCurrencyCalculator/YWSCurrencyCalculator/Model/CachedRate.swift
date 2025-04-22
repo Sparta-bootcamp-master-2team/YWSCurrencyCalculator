9c7f7a3820a374116dcc2e511e454c09934cee4a
_GGDol <bohg6799@naver.com>
2025-04-22 17:55:59 +0900

    feat: CoreData 엔티티 생성 및 파일 명 변경
    - 엔티티 생성
    - 파일 명 변경
    
    Github issue #19

---
+++ YWSCurrencyCalculator/YWSCurrencyCalculator/Model/CachedRate.swift
@@ -0,0 +1,21 @@
+//
+//  FavoriteCurrency.swift
+//  YWSCurrencyCalculator
+//
+//  Created by 양원식 on 4/22/25.
+//
+
+import Foundation
+import CoreData
+
+@objc(CachedRate)
+public class CachedRate: NSManagedObject {
+    @NSManaged public var code: String
+    @NSManaged public var rate: Double
+}
+
+extension CachedRate {
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<CachedRate> {
+        return NSFetchRequest<CachedRate>(entityName: "CachedRate")
+    }
+}
